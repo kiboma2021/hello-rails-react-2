3c9b6c31ecc82c95c40ec1f098126a95580c7279
kiboma <omayioben@gmail.com>
2022-11-22 23:53:42 +0300

    Create react App

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  namespace :api do
+    namespace :v1 do
+      resources :message
+    end
+  end
+  
   get '/greeting_message', to: 'message#greeting_message'
   root "root#index"
 end
